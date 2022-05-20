95e31f1c63651e4f1360c2c2693aab0576e5ef22
Zhentian Wan <zhentian.wan@varian.com>
2022-05-20 12:26:12 +0300

    setup react

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import { DONE, createTimeout, addListener, forOf } from "./libs/broadcasters";
-import {
-  doneAfter,
-  filter,
-  mapTo,
-  repeat,
-  repeatWhen,
-  startWhen,
-  mapSequence,
-} from "./libs/operators";
-import hangman from "./features/hangman";
-import { pipe } from "lodash/fp";
-
-const log = (v) => {
-  console.log(v);
+import { createInterval } from "./libs/broadcasters";
+import {} from "./libs/operators";
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("app");
+const root = createRoot(container);
+const App = () => {
+  const [state, setState] = useState();
+  useEffect(() => {
+    createInterval(1000)(setState);
+  }, []);
+  return <div>{state}</div>;
 };
+root.render(<App />);
